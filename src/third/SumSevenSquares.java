9f3b3ce62f498ad0444e980972729cb0f4c073d6
Ynckson <yannickhuber111@gmx.de>
2023-04-03 18:01:21 +0200

    SumSevenSquares finished

---
+++ src/third/SumSevenSquares.java
@@ -0,0 +1,38 @@
+package third;
+
+import java.lang.Math;
+
+public class SumSevenSquares {
+    //Variablen deklarieren
+    int n;
+    int count;
+
+    //SumSevenSquares Objekt Konstruktor
+    public SumSevenSquares(int n){
+        this.n = n;
+    }
+
+    //Methode zur Berechnung möglicher Lösungen für n
+    public void berechne(){
+        int r = (int)Math.floor(Math.sqrt(n));
+        for(int s = -r; s <= r; s++){
+            for(int t = -r; t <= r; t++){
+                for(int u = -r; u <= r; u++){
+                    for(int v = -r; v <= r; v++){
+                        for(int w = -r; w <= r; w++){
+                            for(int x = -r; x <= r; x++){
+                                for(int y = -r; y <= r; y++){
+                                    if(Math.pow(s,2) + Math.pow(t,2) + Math.pow(u,2) + Math.pow(v,2) + Math.pow(w,2) //Check, ob die Lösung gleich n ist.
+                                            + Math.pow(x,2) + Math.pow(y,2) == n){
+                                        count++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("\nEs gibt genau a(" + n + ")= " + count + " Möglichkeiten, wenn n = " + n);    // Ausgabe Lösungen für n
+    }
+}
---
+++ src/third/SumSevenSquaresMain.java
@@ -0,0 +1,26 @@
+package third;
+
+import java.util.Scanner;
+
+public class SumSevenSquaresMain {
+
+    public static void main(String[] Args){
+        //Variablen deklarieren
+        int n = 0;
+        boolean tryok = true;
+        Scanner sc = new Scanner(System.in);    //Scanner implementieren
+        System.out.println("\nBitte Ganzzahl für n eingeben!");
+        do {
+            try {
+                n = sc.nextInt();   //Wert für n einlesen
+                tryok = true;
+            } catch (Exception e) {     // Falls keine ganze Zahl sondern etwas anderes eingegeben wird,
+                tryok = false;            // soll die Exception abgefangen und eine erneute Eingabe gefordert werden.
+                String s = sc.next();
+                System.out.println("\nFehler!!! Bitte Ganzzahl für n eingeben!");
+            }
+        }while(!tryok);
+        SumSevenSquares s = new SumSevenSquares(n);     // Erstellung eines SumSevenSquares Objekt, welches den Wert n mitbekommt
+        s.berechne();    //Methode zur Berechnung der Möglichkeiten von n
+    }
+}
